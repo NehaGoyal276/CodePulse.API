784a07726efb02e645e4bd8d590472af72714f8e
Neha Goyal <neha.goyal@shipvista.com>
2024-08-05 22:37:31 -0400

    Create POST categories action method, Create Angular Service

---
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+        public String ShortDesc { get; set; }
+        public String Content { get; set; }
+        public String FeaturedImageUrl { get; set; }
+        public String UrlHandle { get; set; }
+        public String Author { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
